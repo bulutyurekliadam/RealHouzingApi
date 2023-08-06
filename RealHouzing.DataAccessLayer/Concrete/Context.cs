dc87f503cfacd6d5615f82febc3da32e1d712c6b
bulutyurekliadam <ayvaz.hakan@outlook.com>
2023-08-06 18:30:31 +0300

    Add project files.

---
+++ RealHouzing.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealHouzing.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealHouzing.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-H67U406;initial catalog=DbRealHouzingApiDb; integrated security =true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
+
+
+//Ders: 22 
+//    40:00 
