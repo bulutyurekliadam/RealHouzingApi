6e8d32cf3c4e97d36a4004eba59a4b5a593cb85c
bulutyurekliadam <ayvaz.hakan@outlook.com>
2023-08-12 18:24:53 +0300

    Api 22. Ders Sonu

--- RealHouzing.ApiLayer/Program.cs
+++ RealHouzing.ApiLayer/Program.cs
@@ -1,6 +1,15 @@
+using RealHouzing.BusinessLayer.Abstract;
+using RealHouzing.BusinessLayer.Concrete;
+using RealHouzing.DataAccessLayer.Abstract;
+using RealHouzing.DataAccessLayer.Concrete;
+using RealHouzing.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Context>();
+builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();  
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
