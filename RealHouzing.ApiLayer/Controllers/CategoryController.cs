745206464f30844c282c2405833ce4eac06aac81
bulutyurekliadam <ayvaz.hakan@outlook.com>
2023-08-13 21:00:27 +0300

    23 ders sonu

--- RealHouzing.ApiLayer/Controllers/CategoryController.cs
+++ RealHouzing.ApiLayer/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RealHouzing.BusinessLayer.Abstract;
+using RealHouzing.DtoLayer.CategoryDtos;
+using RealHouzing.EntityLayer.Concrete;
 
 namespace RealHouzing.ApiLayer.Controllers
 {
@@ -27,5 +29,34 @@ namespace RealHouzing.ApiLayer.Controllers
             _categoryService.TDelete(value);
             return Ok();
         }
+        [HttpPost]
+        public IActionResult AddCategory(ResultCategoryDto resultCategoryDto)
+        {
+            Category category = new Category()
+            {
+                CategoryName = resultCategoryDto.CategoryName
+            };
+            _categoryService.TInsert(category);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            Category category = new Category()
+            {
+                CategoryID = updateCategoryDto.CategoryID,
+                CategoryName = updateCategoryDto.CategoryName
+            };
+            _categoryService.TUpdate(category);
+            return Ok();
+
+        }
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var values = _categoryService.TGetById(id);
+            return Ok(values);
+        }
+        
     }
 }
